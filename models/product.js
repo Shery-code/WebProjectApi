ee8e6b9eac2ff741281ebd0208a4eef06c44797f
Shery-code <71632365+Shery-code@users.noreply.github.com>
2020-12-30 20:43:36 +0500

    commit

---
+++ models/product.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+var productSchema = mongoose.Schema({
+  name: String,
+  price: Number,
+  imageUrl: String,
+});
+var Product = mongoose.model("Product", productSchema);
+
+function validateProduct(data) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    price: Joi.number().min(0).required(),
+    imageUrl: Joi.string().required(),
+  });
+  return schema.validate(data, { abortEarly: false });
+}
+module.exports.Product = Product;
+module.exports.validate = validateProduct;
